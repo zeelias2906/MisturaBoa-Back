73e6c47d2da0703a69458236be5ac24ba7d0fda8
zeelias2906 <js.elias27@hotmail.com>
2025-05-05 16:22:14 -0300

    sei lr6r

--- src/main/java/com/mistura_boa/mistura_boa/models/dtos/UsuarioDTO.java
+++ src/main/java/com/mistura_boa/mistura_boa/models/dtos/UsuarioDTO.java
@@ -1,7 +1,5 @@
 package com.mistura_boa.mistura_boa.models.dtos;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mistura_boa.mistura_boa.models.enums.RoleUsuarioEnum;
 
 import jakarta.validation.constraints.NotBlank;
@@ -19,9 +17,7 @@ public class UsuarioDTO {
 	private Long id;
 	@NotBlank(message = "E-mail deve ser preenchido")
 	private String email;
-	@JsonIgnore
-	private String senha;
-	@JsonIgnore
+	private String senha;	
 	private RoleUsuarioEnum roleUsuario;
 	private PessoaDTO pessoa;
 }
